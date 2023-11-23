bebd4e95ae7748a80a6f168597c72ed59b775cca
Abhinn Krishn <abhinnk21@gmail.com>
2023-11-23 11:59:28 +0530

    FIX: links btn fixed on hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        abril: ["Abril Fatface"],
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        offwhite: "#d6d5c9",
+        bluegreen: "#011627",
+        darkgreen: "#0A100D",
+        greywhite: "#B9BAA3",
+        bloodred: "#902923",
+        scaryblack: "#0D0D0D",
+        terminalgreen: "#1FAA59",
+        zestyorange: "#E07C24",
+        furfle: "#3E8B87",
+        turtle: "#03C6C7",
       },
     },
   },
+
   plugins: [],
-}
+};
